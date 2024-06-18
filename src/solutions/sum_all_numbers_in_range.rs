c22b8f8893b0934480564c4fcdadcc1256328bd7
Prince Muel <vansomecsam@gmail.com>
2024-06-18 01:14:43 +0100

    chore: fix function name (sum_all)

--- src/solutions/sum_all_numbers_in_range.rs
+++ src/solutions/sum_all_numbers_in_range.rs
@@ -2,7 +2,7 @@
 // Return the sum of those two numbers plus the sum of all the numbers between them.
 // The lowest number will not always come first.
 
-// For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+// For example, sum_all([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 
 #[allow(dead_code)]
 pub fn sum_all(arr: &mut [i32; 2]) -> i32 {
